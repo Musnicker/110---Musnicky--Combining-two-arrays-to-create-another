053bae223d337164ba81403999bbec9d911d7a1a
christopher_musnicky <christopher_musnicky@MBA113-039960>
2022-11-01 17:06:31 -0700

    Add project files.

---
+++ 110 - Musnicky- Combining two arrays to create another/110 - Musnicky- Combining two arrays to create another.cpp
@@ -0,0 +1,55 @@
+// 110 - Musnicky- Combining two vectors to create another.cpp :  
+// Author Christopher Musnicky
+// Goal:  Merge two sorted vectors in such a way that the 
+//        resulting vector is also sorted.
+//---------------------------------------------------------
+
+#include <iostream>
+#include <vector> // YAY :)
+#include <iomanip> // Boo :(
+#include <string> // Boo :(
+
+using namespace std;
+
+ 
+ 
+int main()
+{
+    vector <int> a = { 3, 7, 11 };
+    vector <int> b = { 3, 16 };
+    vector <int> c;
+    int i1 = 0;
+    int i2 = 0;
+
+    while (i1 < a.size() && i2 < b.size())
+    {
+        if (a[i1] < b[i2])
+        {
+            c.push_back(a[i1]);
+            i1++;
+        }
+        else
+        {
+            c.push_back(b[i2]);
+            i2++;
+        }
+    }
+    while (i1 < a.size())
+    {
+        c.push_back(a[i1]);
+        i1++;
+    }
+    while (i2 < b.size())
+    {
+        c.push_back(b[i2]);
+        i2++;
+    }
+    for (int num : c) cout << num << ",  ";
+    cout << endl;
+}
+
+
+ 
+
+
+ 
